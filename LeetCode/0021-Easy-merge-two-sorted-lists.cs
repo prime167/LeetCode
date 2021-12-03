6002bc8621e41eda7a5667a3516eff501b6f8457
prime167 <3722845+prime167@users.noreply.github.com>
2021-12-03 16:42:19 +0800

    0021-Easy-merge-two-sorted-lists

---
+++ LeetCode/0021-Easy-merge-two-sorted-lists.cs
@@ -0,0 +1,54 @@
+﻿namespace LeetCode;
+
+public partial class Problems
+{
+    public ListNode MergeTwoLists(ListNode list1, ListNode list2)
+    {
+        var lista = new List<int>();
+        while (list1 != null)
+        {
+            lista.Add(list1.val);
+            list1 = list1.next;
+        }
+
+        while (list2 != null)
+        {
+            lista.Add(list2.val);
+            list2 = list2.next;
+        }
+
+        if (lista.Count == 0)
+        {
+            return null;
+        }
+
+        lista.Sort();
+        ListNode head = new ListNode();
+        ListNode current;
+        head.val = lista[0];
+        head.next = null;
+        current = head;
+        for (int i = 1; i < lista.Count; i++)
+        {
+            ListNode x = new ListNode();
+            x.val = lista[i];
+            x.next = null;
+            current.next = x;
+            current = x;
+        }
+
+        return head;
+    }
+}
+
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
--- Test/UnitTest1.cs
+++ Test/UnitTest1.cs
@@ -1,16 +1,21 @@
 using System;
 using Xunit;
 using LeetCode;
+using System.Collections.Generic;
+using Xunit.Abstractions;
+using System.Collections;
 
 namespace Test;
 
 public class UnitTest1 : IDisposable
 {
     private readonly Problems _problems;
+    private readonly ITestOutputHelper output;
 
-    public UnitTest1()
+    public UnitTest1(ITestOutputHelper output)
     {
         _problems = new Problems();
+        this.output = output;
     }
 
     public void Dispose()
@@ -76,5 +81,83 @@ public class UnitTest1 : IDisposable
     {
         var r1 = _problems.IsValid(str);
         Assert.Equal(expected, r1);
+    }
+
+    [Fact]
+    //[InlineData("(]{}", false)]
+    public void Test0021()
+    {
+        var n1 = new ListNode { val = 1 };
+        var n2 = new ListNode { val = 3 };
+        var n3 = new ListNode { val = 5 };
+        var n4 = new ListNode { val = 7 };
+        var n5 = new ListNode { val = 9 };
+        var a1 = BuildList(n1, n2, n3, n4, n5, n5);
+
+        var n11 = new ListNode { val = 2 };
+        var n12 = new ListNode { val = 4 };
+        var n13 = new ListNode { val = 6 };
+        var n14 = new ListNode { val = 8 };
+        var a2 = BuildList(n11, n12, n13, n14);
+
+        var rr = BuildList(n1, n11, n2, n12, n3, n13, n4, n14, n5);
+        var r1 = _problems.MergeTwoLists(a1,a2);
+        Assert.Equal(rr, r1);
+    }
+
+    public static IEnumerable<object[]> SplitCountData
+    {
+        get
+        {
+            var n1 = new ListNode { val = 1 };
+            var n2 = new ListNode { val = 3 };
+            var n3 = new ListNode { val = 5 };
+            var n4 = new ListNode { val = 7 };
+            var n5 = new ListNode { val = 9 };
+            BuildList(n1, n2, n3, n4, n5, n5);
+
+            var n11 = new ListNode { val = 2 };
+            var n12 = new ListNode { val = 4 };
+            var n13 = new ListNode { val = 6 };
+            var n14 = new ListNode { val = 8 };
+            BuildList(n11, n12, n13, n14);
+            return new List<object[]>
+            {
+                new object[] { "xUnit", 1 },
+                new object[] { "is fun", 2 },
+                new object[] { "to test with", 3 }
+            };
+        }
+    }
+
+    public static ListNode BuildList(params ListNode[] nodes)
+    {
+        if (nodes == null || nodes.Length == 0)
+        {
+            return null;
+        }
+
+        if (nodes.Length == 1)
+        {
+            return nodes[0];
+        }
+
+        ListNode head = new ListNode();
+        ListNode current;
+
+        head.val = nodes[0].val;
+        head.next = null;
+        current = head;
+
+        for (int i = 1; i < nodes.Length; i++)
+        {
+            ListNode x = new ListNode();
+            x.val = nodes[i].val;
+            x.next = null;
+            current.next = x;
+            current = x;
+        }
+
+        return head;
     }
 }
